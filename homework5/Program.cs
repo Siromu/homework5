3d1f14c4803b74b7bc65cda0ccb14daf13f0426e
Siro <89258189+Siromu@users.noreply.github.com>
2022-10-15 11:54:27 +0600

    Add files via upload

---
+++ homework5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.RegularExpressions;
+
+static void Login()
+{
+    Console.Write($"Введите желаемый логин, он должен содержать от 2 до 10 символов. \nИспользуйте только латинские буквы и цифры");
+    string login = Console.ReadLine();
+
+    if (login.Length > 2 && login.Length < 10)
+    {
+        Regex goodLogin = new Regex("^[a-z]${0,9}");
+        if (goodLogin.IsMatch(login))
+        {
+            Console.WriteLine($"У вас хороший логин! Вы ввели: {login}");
+        }
+        else
+        {
+            Console.WriteLine($"Ваш логин не соответствует условиям! Цифра не должна быть первым символом");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Ваш логин меньше двух символов, либо больше десяти!");
+    }
+
+}
+
+Login(); 
